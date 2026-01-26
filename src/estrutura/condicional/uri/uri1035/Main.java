332225574cc7d56895d8021f88b1729f36b84023
Samuel Bitencourt <samuelb2f@outlook.com>
2026-01-26 13:24:18 -0300

    Beecrowd
    - resolvendo exercícios utilizando estruturas condicionais

---
+++ src/estrutura/condicional/uri/uri1035/Main.java
@@ -0,0 +1,22 @@
+package estrutura.condicional.uri.uri1035;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int A, B, C, D;
+
+        A = input.nextInt();
+        B = input.nextInt();
+        C = input.nextInt();
+        D = input.nextInt();
+
+        if (B > C && D > A && (C+D) > (A+B) && C >= 0 && D >= 0 && A % 2 == 0) System.out.println("Valores aceitos");
+        else System.out.println("Valores nao aceitos");
+
+        input.close();
+    }
+}
